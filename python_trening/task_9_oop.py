b5cb39cfff3ab9734aaf3f982bd44b5af1f1234a
war-julia <war-julia@yandex.ru>
2025-07-09 21:32:01 +0300

    eighth commit

--- python_trening/task_9_oop.py
+++ python_trening/task_9_oop.py
@@ -1,6 +1,7 @@
-class Button
+class Button:
 
 
+    type: str = 'Кнопка'
     def __init__(self, text, link):      #атрибуты
         self.text = text                 #аргументы
         self.link = link
@@ -11,10 +12,38 @@ catalog_msk = Button('Каталог', '/msk/catalog')
 
 # получаем доступ к атрибутам
 
-print(home.tex)
+print(home.text)
+print('Кнопка ' + home.text + ' имеет ссылку ' + home.link)
+
+print('\n')
+
+print(catalog_msk.text)
+print('Кнопка ' + catalog_msk.text + ' имеет ссылку ' + catalog_msk.link)
 
 
 
+
+
+class ButtonTwo:
+
+
+
+    def __init__(self, text, link, loc):      #атрибуты
+        self.text = text                 #аргументы
+        self.link = link
+        self.loc = loc
+
+    def click(self):
+        return 'Клик по локатору - {}'.format(self.loc)
+
+
+# создаем экземпляры класса
+home_two = ButtonTwo('Домой', '/home', 'button#home')
+
+
+# вызываем метод
+
+print(home_two.click())
 
 
 
