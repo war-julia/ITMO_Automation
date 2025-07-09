6c3b4228f6dcf89d13b9911e7e844fc4760fe0f9
war-julia <war-julia@yandex.ru>
2025-07-09 16:14:43 +0300

    fifth commit

---
+++ python_trening/task_9_oop.py
@@ -0,0 +1,10 @@
+class Button
+
+
+    def __init__(self, text, link):      #атрибуты
+        self.text = text                 #аргументы
+        self.link = link
+
+# создаем экземпляры класса
+home = Button('Домой', '/home')
+
