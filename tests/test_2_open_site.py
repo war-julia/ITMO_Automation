ec649be2d6222d68173d5da3df1e6f44f886890f
war-julia <war-julia@yandex.ru>
2025-07-10 19:19:48 +0300

    tenth commit

--- tests/test_2_open_site.py
+++ tests/test_2_open_site.py
@@ -1,4 +1,21 @@
+# from selenium import webdriver
+#
+# driver = webdriver.Chrome()  # драйвер
+# driver.get('https://demoqa.com/')
+
+
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()  # драйвер
 driver.get('https://demoqa.com/')
+
+#поиск элемента
+icon = driver.find_element(By.CSS_SELECTOR, 'header > a > img')
+if icon is not None:
+    print('Элемент  не найден')
+else:
+    print('Элемент найден')
+
+
+
